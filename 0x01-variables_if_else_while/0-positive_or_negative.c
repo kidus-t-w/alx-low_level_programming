e939a86570edca867fbf281abc03e4b2d2cc208f
kidus-t-w <kidusk69@gmail.com>
2022-07-05 11:55:42 +0000

    0 v1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,11 +9,17 @@ int main(void)
 
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
-		if(n<0)
+		if(n < 0)
+		{
 			printf("%d is negatieve", n);
-		if(n==0)
+		}
+		if(n == 0)
+		{
 			printf("%d is zero", n);
-		if(n<0)
+		}
+		if(n < 0)
+		{
 			print("%d is positive", n);
+		}
 		return(0);
 }
