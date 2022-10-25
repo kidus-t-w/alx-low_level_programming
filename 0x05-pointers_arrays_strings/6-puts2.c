73b01694d0cc6c87cc04f380f3ecaf7b7421d785
kidus-t-w <64226591+kidus-t-w@users.noreply.github.com>
2022-10-25 21:09:13 +0300

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *puts2- print skip one number
+ *@str:checked
+ *Return:nothing
+ */
+void puts2(char *str)
+{
+
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
