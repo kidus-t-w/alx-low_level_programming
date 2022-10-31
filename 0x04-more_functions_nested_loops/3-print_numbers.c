32d7be52e0d1507088ee4b2ed852012c3bdfd2d7
Kidus-t-w <kidusk69@gmail.com>
2022-10-31 07:15:19 +0000

    v1

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - print digits 
+ * print_numbers - print digits
  *
  * Return: Always 0
  */
 void print_numbers(void)
 {
 	int n;
-	
+
 	for (n = '0'; n <= '9'; n++)
 	{
 		_putchar(n);
