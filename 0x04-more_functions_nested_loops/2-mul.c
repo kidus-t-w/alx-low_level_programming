66ae2cfdbc32e9e1c7e45e23aab5d66b2b95f5fd
Kidus-t-w <kidusk69@gmail.com>
2022-10-31 06:58:14 +0000

    v1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply a and b.
+ * @a: first number.
+ * @b: second number.
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int m;
+	m = a * b;
+	return (m);
+}
