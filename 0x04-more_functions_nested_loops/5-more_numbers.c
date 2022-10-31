8d8e6d802a3a206dd5f3b331c3e886cd98eefdb0
Kidus-t-w <kidusk69@gmail.com>
2022-10-31 08:57:04 +0000

    v2

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+				_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
