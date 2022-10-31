9a3ce2fcefafccaf24b37c1d51ef743a3aa4d65d
Kidus-t-w <kidusk69@gmail.com>
2022-10-31 06:31:49 +0000

    v2

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
+/*
+ * int _isupper(int c)
+ *
+ * Return 1
+ */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
-		return (0);
-	} else
 		return (1);
+	} else
+		return (0);
 }
