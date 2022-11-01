bbe9b3bd13f1869c10125467f378a88df1d81465
Kidus-t-w <kidusk69@gmail.com>
2022-11-01 08:37:25 +0000

    v1

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - goint to print fizz.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else if (x % 3  == 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else
+		{
+		printf("%d ", x);
+		}
+	}
+	printf("\n");
+	return (0);
+}
