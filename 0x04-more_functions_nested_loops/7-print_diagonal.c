af53f459a245717a5c95ac468947c24a9379cbcb
Kidus-t-w <kidusk69@gmail.com>
2022-11-01 06:56:26 +0000

    v1

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal.
+ * @n: number of diagonals .
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int x, space;
+
+	for (x = 1; x <= n; x++)
+	{
+		if (n == 0)
+		{
+			_putchar('\n');
+		}
+		for (space = 1; space <= x ; space++)
+		{
+		_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
