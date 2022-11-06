43950f37e0126454f014ccbbc1a6fd86b68f5598
Kidus-t-w <kidusk69@gmail.com>
2022-11-06 06:04:54 +0000

    Function that changes the number

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,12 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *reset_to_98- it reset to 98
- *@n: checked
- *Return: nothing
+ * reset_to_98 - function name 
+ * @n: is the pointer 
+ * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
-	int *ptr;
 
-	ptr = n;
-
-	*ptr = 98;
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/README.md
+++
@@ -1,5 +0,0 @@
-Am going to tell you how to point to the point by the point
-These are the tools
-Arrays
-Pointers
-Strings
