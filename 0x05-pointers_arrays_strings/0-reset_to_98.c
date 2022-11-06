262cefc0bd75709a7a1c678f1e18f870782804f0
Kidus-t-w <kidusk69@gmail.com>
2022-11-06 06:07:42 +0000

    Solved trailing white space

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - function name 
- * @n: is the pointer 
+ * reset_to_98 - function name
+ * @n: is the pointer
  * Return: Always 0.
  */
 void reset_to_98(int *n)
