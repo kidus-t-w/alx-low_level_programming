0d2748a654ce21db742e9116fb9ce9c479d9ac50
Kidus-t-w <kidusk69@gmail.com>
2022-11-06 12:53:06 +0000

    Added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ * @argc: the number of command line arguments
+ * @argv: the array that holds the command lines argument
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
