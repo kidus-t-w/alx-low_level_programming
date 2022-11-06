082c43543f30c2b3a9eefe68456739528c8cc6c7
Kidus-t-w <kidusk69@gmail.com>
2022-11-06 06:17:26 +0000

    String counting

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - mainfunction
+ * @s: input string 
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		;
+	}
+	return (x);
+}
