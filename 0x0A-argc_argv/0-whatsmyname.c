e146ab6006b494184dc7ab62fbea0b0679aa59de
Kidus-t-w <kidusk69@gmail.com>
2022-11-06 12:43:44 +0000

    Added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: the number of command line arguments
+ * @argv: array tha holds the commmand line arguments as strings.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	
+	printf("%s\n", argv[0]);
+	return (0);
+}
