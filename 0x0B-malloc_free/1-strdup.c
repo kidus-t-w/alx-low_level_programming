c58eefd940d9398c5e53b4f45870b7bddce5019f
Kidus-t-w <kidusk69@gmail.com>
2022-11-10 12:47:19 +0000

    Attempted to pass checker for 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 
 /**
  * _strdup - Points to a newly allcoated space in memory
- * @str: strint to be 
+ * @str: strint to be
  *
  * Return: On success, the function returns string. On
  * Error NULL.
@@ -12,13 +12,16 @@ char *_strdup(char *str)
 	char *ch;
 	int x, y;
 	
+	if (str == NULL)
+	       return (NULL);
 	for (x = 0; str[x] != '\0'; x++)
 	{
 		;
 	}
+	x = x + 1;
 	ch = malloc(sizeof(char) * x);
-	if (str == NULL)
-	       return (NULL);
+	if (ch == NULL)
+		return (NULL);
 	for (y = 0; y <= x; y++)
 	{
 		ch[y] = str[y];
