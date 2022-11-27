6e99a3f10e62ee875fb4f19d2b99a8ee4d2c212f
Kidus-t-w <kidusk69@gmail.com>
2022-11-27 08:32:11 +0000

    Added 1-lists_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: head node
+ * Return: number of elements in a linked list
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		if (h->str != NULL)
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
