1d2e350f1df3b6a9f8da1c35a4f4799f50239e7a
kidus-t-w <kidusk69@gmail.com>
2023-07-17 09:32:44 +0300

    edited the task 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,11 @@
-#include "MAIN.H"
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points to to 98.
+ * @ *n: value taken
+ * 
+ * Return: returns updated value
+*/
 
 void reset_to_98(int *n)
 {
