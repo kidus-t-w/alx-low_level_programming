fb5b55e9fb1ea51ce4fc73ef9b9a3813e327c9ae
kidus-t-w <kidusk69@gmail.com>
2023-07-17 11:07:53 +0300

    Added task 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: points to a string
+ * Returns: returns nothing.
+*/
+
+void _puts(char *str)
+{
+    int x;
+    x = 0;
+
+    while (str[x] != '\0')
+    {
+        putchar(str[x]);
+        x++;
+    }
+}
