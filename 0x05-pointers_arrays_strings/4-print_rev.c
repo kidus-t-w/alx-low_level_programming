95eb4eb9fa800eecf3092790463e8303b4d47f17
kidus-t-w <kidusk69@gmail.com>
2023-07-17 11:28:40 +0300

    Added task 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse.
+ * @s: pointer to a string
+ * Return: returns void
+*/
+
+void print_rev(char *s)
+{
+    int x, y;
+
+    x = 0;
+    while (s[x] != '\0')
+    {
+        x++;
+    }
+    for (y = x; y >= 0; y--)
+    {
+        putchar(s[y]);
+    }
+    putchar('\n');
+}   
