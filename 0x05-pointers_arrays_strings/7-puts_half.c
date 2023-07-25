5e67ecbde5aa49e358db4e7e7bccce686f36c26d
kidus-t-w <kidusk69@gmail.com>
2023-07-25 11:13:42 +0300

    Added 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half- prints half of a string
+ * @str: pointer to a character
+ * Return: return half of a string
+*/
+
+void puts_half(char *str)
+{
+	int x, y;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		;
+	}
+	y = x / 2;
+	if (x % 2 != 0)
+	{
+		y = (x - 1) / 2;
+	}
+	while (y < x)
+	{
+		putchar(str[y]);
+		y++;
+	}
+}
