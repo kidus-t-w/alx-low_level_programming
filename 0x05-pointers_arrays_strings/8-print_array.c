8bd6b8a855068da2fdd90099aae24f221c642ee3
kidus-t-w <kidusk69@gmail.com>
2023-07-25 11:31:37 +0300

    Added 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array- prints n elements of an array of integers
+ * @a: pointer to an integer
+ * @n: number of elements of the array ot be printed
+*/
+
+void print_array(int *a, int n)
+{
+	int x;
+
+	x = 0
+	while (x < n)
+	{
+		putchar(a[x],","," ");
+		x++;
+	}
+}
