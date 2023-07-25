1c0bad18f5f01ddbd5e9c80169c26d214cb78c78
kidus-t-w <kidusk69@gmail.com>
2023-07-25 10:37:57 +0300

    updated 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,16 +8,16 @@
 
 void puts2(char *str)
 {
-    int x;
+	int x;
 
-    x = 0;
-    while (str[x] != '\0')
-    {
-        if (x % 2 == 0)
-        {
-            putchar(str[x]);
-        }
-        x++;
-    }
-    putchar('\n');
+	x = 0;
+	while (str[x] != '\0')
+	{
+		if (x % 2 == 0)
+		{
+			putchar(str[x]);
+		}
+		x++;
+	}
+	putchar('\n');
 }
