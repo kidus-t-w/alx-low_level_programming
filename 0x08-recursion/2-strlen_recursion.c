bb51e6bef84dd91d5d2e0acd0ee864440aa3f7c1
kidus-t-w <kidusk69@gmail.com>
2023-07-28 14:35:16 +0300

    updated 2-strlen_recursio.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,12 +8,9 @@
 
 int _strlen_recursion(char *s)
 {
-    int len;
-    if (*s == '\0')
-    {
-        return (len);
-    }
-    len = 0;
-    len++;
-    _strlen_recursion(s+1);
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
 }
