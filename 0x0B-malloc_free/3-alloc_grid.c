654de6c7e462b16a7365c164cadf217429f107e7
kidus-t-w <kidusk69@gmail.com>
2023-09-25 11:17:57 +0300

    Added files

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * alloc_grid- returns a pointer to a 2 dimensional array
+ * @width: width of the array
+ * @height: height of the array 
+ * Return: on success returns pointer to an array
+*/
+
+int **alloc_grid(int width, int height)
+{
+    
+}
